1dead21ca1ded8cdaae194c594426219c23ff49e
Maxime Meignan <Maxime.Meignan@ensimag.grenoble-inp.fr>
2014-05-04 22:26:15 +0200

    Squelette traitement commande arduino

--- arduino/ProjetSmartCase.ino
+++ arduino/ProjetSmartCase.ino
@@ -19,6 +19,21 @@ String lireCommande(){
   }
 }  
 
+int lireCapteur (int numCapteur){
+  Serial.println("On lit la valeur du capteur " + String(numCapteur));
+  return 1337;
+}
+
+int ouvrirSerrure (int numSerrure){
+  Serial.println("On ouvre la serrure " + String(numSerrure));
+  return 0;
+}
+
+int fermerSerrure (int numSerrure){
+  Serial.println("On ferme la serrure " + String(numSerrure));
+  return 0;
+}
+
 String commandeLue;
 void setup(){
   Serial.begin(9600);
@@ -30,8 +45,54 @@ void loop(){
     commandeLue = lireCommande();
   } 
   Serial.print(commandeLue);
+
+  switch (commandeLue.charAt(0)){
+
+    //Commande "capteur"
+  case 'c':
+    {
+      int numeroCapteur = commandeLue.substring(1).toInt();
+      int valeurCapteur = lireCapteur(numeroCapteur);
+      Serial.println(String(valeurCapteur));
+      break;
+    }
+    //Commande "ouverture serrure"
+  case 'o':
+    {
+      int numeroSerrure = commandeLue.substring(1).toInt();
+      int resultat = ouvrirSerrure(numeroSerrure);
+      if (resultat != 0){
+        //traiter l'erreur
+        Serial.println("erreur");
+      }
+      break;
+    }
+    //Commande "fermeture serrure"
+  case 'f':
+    {
+      int numeroSerrure = commandeLue.substring(1).toInt();
+      int resultat = fermerSerrure(numeroSerrure);
+      if (resultat != 0){
+        //traiter l'erreur
+        Serial.println("erreur");  
+      }
+      break;
+    }
+
+  default:
+    //traiter l'erreur
+    Serial.println("erreur");
+    break;
+  }
+
+
   commandeLue="";
 }
+
+
+
+
+
 
 
 
