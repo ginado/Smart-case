6edef68fd39ffb102e395c9c81590e012a2e6532
Hugo Locteau <Hugo.Locteau@ensimag.imag.fr>
2014-05-07 23:07:40 +0200

    Essai de scala

--- SmartCaseApp/app/controllers/MainController.java
+++ SmartCaseApp/app/controllers/MainController.java
@@ -6,7 +6,6 @@ import play.mvc.Result;
 public class MainController extends Controller {
     
     public static Result index() {
-        return ok(views.html.index.render("Hello from Java"));
+        return ok(views.html.index.render("Hello From Java");
     }
-    
 }
