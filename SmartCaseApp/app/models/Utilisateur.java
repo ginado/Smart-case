8cba67624487169e11d01474b260dcf75f6cd591
Bombrun Thomas <thomas.bombrun@grenoble-inp.fr>
2014-05-07 21:27:14 +0200

    remplissage d utilisateur

--- SmartCaseApp/app/models/Utilisateur.java
+++ SmartCaseApp/app/models/Utilisateur.java
@@ -5,5 +5,42 @@ package models;
  * @author bombrunt
  */
 public class Utilisateur {
+    private String adresse_mail;
+    private String prenom;
+    private String nom;
+    private String hash_password;
+    private int credit;
+
+    public Utilisateur(String adresse_mail, String prenom, String nom, String hash_password, int credit) {
+        this.adresse_mail = adresse_mail;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.hash_password = hash_password;
+        this.credit = credit;
+    }
+
+    public String getAdresse_mail() {
+        return adresse_mail;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getHash_password() {
+        return hash_password;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+    
+    
     
 }
+
+
