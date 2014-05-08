fb3a87a526b972e6d9fb6e97db11e5af1387a19c
ginado <dobrescu.gina.adriana@gmail.com>
2014-05-08 20:01:36 +0200

    view

--- SmartCaseApp/app/controllers/MainController.java
+++ SmartCaseApp/app/controllers/MainController.java
@@ -3,16 +3,12 @@ import play.api.*;
 import scala.*;
 
 
-import dao.CasierDao;
 import dao.UtilisateurDAO;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import models.Utilisateur;
-import models.Casier;
 import play.mvc.Controller;
-import java.util.ArrayList;
-import java.util.Collection;
 import play.mvc.Result;
 
 public class MainController extends Controller {
@@ -28,12 +24,4 @@ public class MainController extends Controller {
         return ok(views.html.index.render("Hello from " + user.getPrenom()));
     }
     
-    public static Result index2(){
-      try{
-        return ok(views.html.casier_tab.render(CasierDao.getCasiers()));
-     } catch (SQLException ex){
-            return ok(views.html.index.render(ex.getMessage()));
-    }    
-}
-   
 }
