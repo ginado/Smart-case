df529f94c6b217c9c87bf93ec3cba7e2139ab711
Thomas Bombrun <thomas.bombrun@ensimag.grenoble-inp.fr>
2014-05-09 21:00:58 +0200

    Ajout dun gestionnaire de session
    utilise flexjson pou lier des objets java a la session html

---
+++ SmartCaseApp/app/controllers/SessionManager.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import play.Logger;
+import play.mvc.Http;
+import play.mvc.Http.Session;
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+
+public class SessionManager {
+
+private static JSONSerializer s = new JSONSerializer();
+
+    public static void addSession(String key, Object value) {
+
+        if(value != null) {
+            Session session = Http.Context.current().session();
+            session.put(key, s.deepSerialize(value));
+        } else {
+            Logger.info("Value for " + key + " is null");
+        }
+    }
+
+    public static <T> T get(String key) {
+
+        Session session = Http.Context.current().session();
+        final String value = session.get(key);
+
+        if (value == null) {
+            return null;
+        }
+
+        return new JSONDeserializer<T>().deserialize(value);
+    }
+    
+    public static void closeSession(){
+        Http.Context.current().session().clear();
+    }
+
+}
