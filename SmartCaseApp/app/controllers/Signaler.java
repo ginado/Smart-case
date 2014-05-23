118c7250ae35588fb484454f5e10b15905aa7262
Thomas Bombrun <thomas.bombrun@ensimag.grenoble-inp.fr>
2014-05-23 15:08:47 +0200

    Correctio du controller Signaler

--- SmartCaseApp/app/controllers/Signaler.java
+++ SmartCaseApp/app/controllers/Signaler.java
@@ -6,6 +6,7 @@ package controllers;
 
 import dao.CasierDao;
 import dao.TransactionDao;
+import dao.UtilisateurDAO;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Calendar;
@@ -24,28 +25,34 @@ import utils.SessionManager;
 public class Signaler extends Controller {
     
     public static Result choisir(){
+        boolean connecte= SessionManager.get("utilisateur")!=null;
         try {
             Collection<Casier> casiers = CasierDao.getCasiers();
-            return ok(views.html.choix_casier.render(casiers,"Choississez le casier dont vous voulez signaler le contenu comme étant nuisible.","Signaler","signaler"));
+            if(Casier.allAreEmpty(casiers)) {
+                if(connecte) {
+                    return ok(views.html.accueil.render(UtilisateurDAO.getUtilisateur(SessionManager.get("utilisateur")),"Tout les casiers sont vides."));
+                } else {
+                    return ok(views.html.index.render("Tout les casiers sont vides."));
+                }
+            } else {
+                return ok(views.html.choix_casier.render(casiers,"Choississez le casier dont vous voulez signaler le contenu comme étant nuisible.","Signaler","signaler"));
+            }
         } catch (SQLException ex) {
-            return ok(views.html.error.render("Erreur interne","/main"));
+            return ok(views.html.error.render("Erreur interne",connecte ? "/main" :"/"));
             
         }
     }
     
     
     public static Result signaler(String idCasier){
+        boolean connecte= SessionManager.get("utilisateur")!=null;
         java.sql.Date date = new Date(Calendar.getInstance().getTimeInMillis());
         try {
-            TransactionDao.ajouterTransaction(new Transaction(0, date, "signaler", "anonymous", Integer.parseInt(idCasier)));
+            TransactionDao.ajouterTransaction(new Transaction(0, date, "signaler", SessionManager.get("utilisateur"), Integer.parseInt(idCasier)));
         } catch (SQLException ex) {
-            if(SessionManager.get("utilisateur")==null) {
-                return ok(views.html.error.render("Erreur interne","/"));
-            } else {
-                return ok(views.html.error.render("Erreur interne","/main"));
-            }
+            return ok(views.html.error.render("Erreur interne",connecte ? "/main" :"/"));
         }
-        return redirect("/main");
+        return redirect(connecte ? "/main" :"/");
     }
     
 }
